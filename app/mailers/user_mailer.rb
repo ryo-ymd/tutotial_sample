34e0cc463b75598adb765fb7ee3f270eb831bc06
ryo-ymd <rylice.dym@gmail.com>
2016-08-17 15:27:01 +0900

    パスワード初期化とメール設定の追加

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,9 +1,8 @@
 class UserMailer < ApplicationMailer
 
-  def password_reset
-    @greeting = "Hi"
-
-    mail to: "to@example.org"
+  def password_reset(user)
+    @user = user
+    mail to: user.email, subject: "Password reset"
   end
 
   def account_activation(user)
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ActiveRecord::Base
   before_create :create_activation_digest
   before_save :downcase_email
-  attr_accessor :remember_token, :activation_token
+  attr_accessor :remember_token, :activation_token, :reset_token
 
 
   before_save { self.email = email.downcase }
@@ -44,6 +44,20 @@ class User < ActiveRecord::Base
 
   def send_activation_mail
     UserMailer.account_activation(self).deliver_now
+  end
+
+  def create_reset_digest
+    self.reset_token = User.new_token
+    update_attribute(:reset_digest,  User.digest(reset_token))
+    update_attribute(:reset_sent_at, Time.zone.now)
+  end
+
+  def send_password_reset_email
+    UserMailer.password_reset(self).deliver_now
+  end
+
+  def password_reset_expired?
+    reset_sent_at < 2.hours.ago
   end
 
   private
