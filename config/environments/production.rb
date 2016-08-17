90924f12d8f77175a2828f07a2a0c47a2e414ccd
ryo-ymd <rylice.dym@gmail.com>
2016-08-17 15:49:41 +0900

    Typo

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -80,7 +80,7 @@ Rails.application.configure do
   # Heroku Email
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
-  host = 'v-r-tutorial-sampleherokuapp.com'
+  host = 'v-r-tutorial-sample.herokuapp.com'
   config.action_mailer.default_url_options = { host: host }
   ActionMailer::Base.smtp_settings = {
       :address        => 'smtp.sendgrid.net',
